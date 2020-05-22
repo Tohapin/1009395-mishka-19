977ec395259bb4be8c184e6671bd2f5996f2f58f
Anton Pinchukov <andromoom@gmail.com>
2020-05-22 18:11:27 +0300

    Автоматизируем сборку личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -27,16 +27,17 @@ gulp.task("sprite", function () {
 
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
- .pipe(imagemin([
- imagemin.optipng({optimizationLevel: 3}),
- imagemin.svgo()
- ]))
- .pipe(gulp.dest("build/img"));
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
-  .pipe(gulp.dest("source/img"));
+  pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("css", function () {
